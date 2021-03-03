bad665c7b5f5cd5a2e458504b8e353f2b28ea5e6
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-03-03 23:05:57 +0300

    should work as intended now

--- index.js
+++ index.js
@@ -7,14 +7,9 @@ const command = process.argv[2];
 const unit = process.argv[3];
 const component = process.argv[4];
 
-/* if (command !== "g" || unit !== "c" || !component) {
+if (command !== "g" || unit !== "c" || !component) {
     throw "You might want to read the manual, your command made no sense.";
-} */
-
-console.log(process.argv);
-console.log(__dirname);
-console.log(process.cwd());
-console.log(process.chdir());
+}
 
 /* 
 const folderPath =
@@ -23,7 +18,7 @@ const folderPath =
         .filter((undefined, i, { length }) => i !== length - 1)
         .join("/") || "src"; */
 
-/* const componentName = component
+const componentName = component
     .split("/")
     .filter((undefined, i, { length }) => i === length - 1)
     .map((i) =>
@@ -34,11 +29,12 @@ const folderPath =
     .join();
 
 const componentFolder = `src/${component}`;
+const currentWorkingDirectory = process.cwd();
 
-fs.mkdirSync(path.join(__dirname, `${componentFolder}`), { recursive: true });
+fs.mkdirSync(path.join(currentWorkingDirectory, `${componentFolder}`), { recursive: true });
 
 fs.writeFileSync(
-    path.join(__dirname, `${componentFolder}/${componentName}.tsx`),
+    path.join(currentWorkingDirectory, `${componentFolder}/${componentName}.tsx`),
     `import { FC } from "react";
     
 import { ${componentName}Props } from "./${componentName}.model";
@@ -50,17 +46,17 @@ export const ${componentName}: FC<${componentName}Props> = () => (
 );
 
 fs.writeFileSync(
-    path.join(__dirname, `${componentFolder}/index.ts`),
+    path.join(currentWorkingDirectory, `${componentFolder}/index.ts`),
     `export { ${componentName} } from "./${componentName}";`
 );
 
 fs.writeFileSync(
-    path.join(__dirname, `${componentFolder}/${componentName}.model.ts`),
+    path.join(currentWorkingDirectory, `${componentFolder}/${componentName}.model.ts`),
     `export interface ${componentName}Props {}`
 );
 
 fs.writeFileSync(
-    path.join(__dirname, `${componentFolder}/${componentName}.module.scss`),
+    path.join(currentWorkingDirectory, `${componentFolder}/${componentName}.module.scss`),
     ""
 );
- */
+
